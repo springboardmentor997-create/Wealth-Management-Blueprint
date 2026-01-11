eeee9a918c99d9de6bc90f957a78cca16a2e41c2
Nakul S Kumar <nakulsuresh35@gmail.com>
2026-01-11 21:35:01 +0530

    front end changes for deployment

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // 1. Check if we are in production (Netlify) or local
-const backendUrl = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+const backendUrl = 'https://wealth-plan-api.onrender.com';
 
 const client = axios.create({
   baseURL: backendUrl, 
